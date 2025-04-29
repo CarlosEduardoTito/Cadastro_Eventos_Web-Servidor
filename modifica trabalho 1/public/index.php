684af58ef828fffa961ae37f31d416d6a7424140
Beatriz Silva <Beatrizkauany22@gmail.com>
2025-04-29 12:15:07 -0300

    Update index.php

--- modifica trabalho 1/public/index.php
+++ modifica trabalho 1/public/index.php
@@ -2,8 +2,12 @@
 session_start();
 
 require_once '../app/controllers/AuthController.php';
+require_once '../app/controllers/EventoController.php';
+require_once '../app/controllers/ReservaController.php';
 
 $auth = new AuthController();
+$eventoController = new EventoController();
+$reservaController = new ReservaController();
 
 $action = $_GET['action'] ?? null;
 
@@ -18,6 +22,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         case 'cadastrar':
             $auth->cadastrar($_POST['nome'], $_POST['email'], $_POST['senha']);
             break;
+        case 'criar_evento':
+            $eventoController->criar($_POST);
+            break;
+        case 'reservar':
+            $reservaController->reservar($_SESSION['usuario']['id'], $_POST['evento_id'], $_POST['quantidade']);
+            break;
+        case 'cancelar_reserva':
+            $reservaController->cancelar($_POST['reserva_id']);
+            break;
         default:
             echo "Ação não encontrada.";
             break;
@@ -29,6 +42,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             break;
         case 'cadastrar':
             include '../app/views/auth/Cadastrar.php';
+            break;
+        case 'criar_evento':
+            include '../app/views/eventos/Criar.php';
+            break;
+        case 'listar_eventos':
+            include '../app/views/eventos/Listar.php';
+            break;
+        case 'reservar':
+            include '../app/views/reservas/Reservar.php';
+            break;
+        case 'minhas_reservas':
+            include '../app/views/reservas/MinhasReservas.php';
             break;
         case 'menu':
             include '../app/views/partials/Menu.php';
