c3b2ded7257e2dfb97cf084ba40b3cf387269f26
Beatriz Silva <Beatrizkauany22@gmail.com>
2025-04-29 11:48:07 -0300

    Update session.php

--- modifica trabalho 1/app/config/session.php
+++ modifica trabalho 1/app/config/session.php
@@ -4,4 +4,12 @@ session_start();
 if (!isset($_SESSION['usuarios'])) {
     $_SESSION['usuarios'] = [];
 }
+
+if (!isset($_SESSION['eventos'])) {
+    $_SESSION['eventos'] = [];
+}
+
+if (!isset($_SESSION['reservas'])) {
+    $_SESSION['reservas'] = [];
+}
 ?>
