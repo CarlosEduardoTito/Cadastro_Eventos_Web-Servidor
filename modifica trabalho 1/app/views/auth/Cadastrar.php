df9941b8deb768e8e79f863040969aa89dc2ce7c
CarlosTito <158493851+CarlosEduardoTito@users.noreply.github.com>
2025-04-29 14:09:30 -0300

    Update Cadastrar.php

--- modifica trabalho 1/app/views/auth/Cadastrar.php
+++ modifica trabalho 1/app/views/auth/Cadastrar.php
@@ -1,24 +1,16 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Cadastrar</title>
-</head>
-<body>
-<h2>Cadastrar</h2>
+<h2 class="form-title">Cadastrar</h2>
 <?php if (!empty($_SESSION['erro'])): ?>
-    <p style="color:red;"><?= $_SESSION['erro']; unset($_SESSION['erro']); ?></p>
+    <p class="error-message"><?= $_SESSION['erro']; unset($_SESSION['erro']); ?></p>
 <?php endif; ?>
-<form action="/trabalho1/public/index.php?action=cadastrar" method="POST">
-    Nome: <input type="text" name="nome" required><br>
-    Email: <input type="email" name="email" required><br>
-    Senha: <input type="password" name="senha" required><br>
-    <button type="submit">Cadastrar</button>
+<form action="/trabalho1/public/index.php?action=cadastrar" method="POST" class="form-container">
+    <label for="nome">Nome:</label>
+    <input type="text" id="nome" name="nome" required class="form-input"><br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required class="form-input"><br>
+    <label for="senha">Senha:</label>
+    <input type="password" id="senha" name="senha" required class="form-input"><br>
+    <button type="submit" class="form-button">Cadastrar</button>
 </form>
-<a href="/trabalho1/public/index.php?action=login">Voltar para Login</a>
-</body>
-</html>
+<p class="form-link">
+    <a href="/trabalho1/public/index.php?action=login">Voltar para Login</a>
+</p>
