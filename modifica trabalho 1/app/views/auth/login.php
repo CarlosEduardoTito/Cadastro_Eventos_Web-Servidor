67f8cbbbb366da0429c174dfebc18d429334e635
CarlosTito <158493851+CarlosEduardoTito@users.noreply.github.com>
2025-04-29 14:09:58 -0300

    Update login.php

--- modifica trabalho 1/app/views/auth/login.php
+++ modifica trabalho 1/app/views/auth/login.php
@@ -1,10 +1,14 @@
-<h2>Login</h2>
+<h2 class="form-title">Login</h2>
 <?php if (!empty($_SESSION['erro'])): ?>
-    <p style="color:red;"><?= $_SESSION['erro']; unset($_SESSION['erro']); ?></p>
+    <p class="error-message"><?= $_SESSION['erro']; unset($_SESSION['erro']); ?></p>
 <?php endif; ?>
-<form action="/trabalho1/public/index.php?action=login" method="POST">
-    Email: <input type="email" name="email" required><br>
-    Senha: <input type="password" name="senha" required><br>
-    <button type="submit">Entrar</button>
+<form action="/trabalho1/public/index.php?action=login" method="POST" class="form-container">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required class="form-input"><br>
+    <label for="senha">Senha:</label>
+    <input type="password" id="senha" name="senha" required class="form-input"><br>
+    <button type="submit" class="form-button">Entrar</button>
 </form>
-<a href="/trabalho1/public/index.php?action=cadastrar">Cadastrar-se</a>
+<p class="form-link">
+    <a href="/trabalho1/public/index.php?action=cadastrar">Cadastrar-se</a>
+</p>
