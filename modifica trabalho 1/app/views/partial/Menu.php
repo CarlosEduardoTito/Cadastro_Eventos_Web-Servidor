12e84b84f4d72562b0c81f2109ce5d1a7e7eb16c
CarlosTito <158493851+CarlosEduardoTito@users.noreply.github.com>
2025-04-29 13:14:59 -0300

    Update Menu.php

--- modifica trabalho 1/app/views/partial/Menu.php
+++ modifica trabalho 1/app/views/partial/Menu.php
@@ -1,5 +1,17 @@
 <nav style="margin-bottom:20px;">
     <form action="/trabalho1/public/index.php" method="GET" style="display:inline;">
+        <input type="hidden" name="action" value="listar_eventos">
+        <button type="submit">Eventos</button>
+    </form>
+    <form action="/trabalho1/public/index.php" method="GET" style="display:inline;">
+        <input type="hidden" name="action" value="minhas_reservas">
+        <button type="submit">Minhas Reservas</button>
+    </form>
+    <form action="/trabalho1/public/index.php" method="GET" style="display:inline;">
+        <input type="hidden" name="action" value="criar_evento">
+        <button type="submit">Criar Evento</button>
+    </form>
+    <form action="/trabalho1/public/index.php" method="GET" style="display:inline;">
         <input type="hidden" name="action" value="logout">
         <button type="submit">Sair</button>
     </form>
